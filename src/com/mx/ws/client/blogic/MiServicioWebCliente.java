d5a6ce1defba88b0216d97a5a69ea386b380ffb6
smarv2 <HP Compaq 6005 Pro@POSEIDON>
2018-03-31 12:00:43 -0600

    Ajustes en manejo de errores

--- src/com/mx/ws/client/blogic/MiServicioWebCliente.java
+++ src/com/mx/ws/client/blogic/MiServicioWebCliente.java
@@ -5,6 +5,8 @@
  */
 package com.mx.ws.client.blogic;
 
+import com.mx.ws.client.utilerias.Utilerias;
+import java.util.Date;
 import org.apache.log4j.Logger;
 
 /**
@@ -12,25 +14,36 @@ import org.apache.log4j.Logger;
  * @author Mario Ramirez
  */
 public class MiServicioWebCliente {
-    
+
     /**
-    * Campo LOG de tipo Logger.
-    */
+     * Campo LOG de tipo Logger.
+     */
     protected static final Logger LOG = Logger.getLogger(MiServicioWebCliente.class);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        LOG.info("<<Inicia proceso \"Mi Servicio Web Cliente\" "
+                + Utilerias.dateToString(new Date(), "dd/MM/yyyy HH:mm:ss") + ">>");
+
+        try {
+            miServicioWebCliente();
+        } catch (Exception e) {
+            LOG.error("Error en \"MI Servicio Web Cliente\"", e);
+        }
+        
+        LOG.info("<<Termina proceso \"Mi Servicio Web Cliente\" "
+                + Utilerias.dateToString(new Date(), "dd/MM/yyyy HH:mm:ss") + ">>");
+
+    }
+
+    public static void miServicioWebCliente() {
         String nombre;
         String saludo = null;
         nombre = "Mario Alan Ramirez Vazquez";
-        try {
-            saludo = saluda(nombre);    
-        } catch (Exception e) {
-            LOG.error("Error en la respuesta del WS.", e);
-        }
-        
+        saludo = saluda(nombre);
         LOG.info(saludo);
     }
 
@@ -39,5 +52,5 @@ public class MiServicioWebCliente {
         com.mx.ws.client.cliente.MiServicioWeb port = service.getMiServicioWebPort();
         return port.saluda(arg0);
     }
-    
+
 }
